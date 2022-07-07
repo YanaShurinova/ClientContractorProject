f01cb062c69bde4aa9cbf6975140089e4d20d775
YanaShurinova <YanaShurinova009@gmail.com>
2022-07-07 13:32:22 +0400

    Почта(валидация)

--- src/main/java/com/company/clientcontractorproject/app/IndividualService.java
+++ src/main/java/com/company/clientcontractorproject/app/IndividualService.java
@@ -1,9 +1,11 @@
 package com.company.clientcontractorproject.app;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class IndividualService {
+
     public boolean isCorrectEmail(String email){
         return email.matches("[0-9]*[A-Za-z]+\\.*\\@[A-Za-z]+\\.[A-Za-z]+");
     }
